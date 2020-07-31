827c990c0aec6c89605bdef657c769637d6cd6a4
Fabio Colognesi <fabio.colognesi@techspell.eu>
2020-07-31 11:13:59 +0200

    Added missing files.

---
+++ pdm/reports/mrp_report_bom_structure.py
@@ -0,0 +1,119 @@
+from odoo import api, models, _
+from odoo.tools import float_round
+
+class ReportBomStructure(models.AbstractModel):
+    _inherit = 'report.mrp.report_bom_structure'
+
+### OVERRIDDEN STANDARD METHODS 
+
+    def _get_bom_lines(self, bom, bom_quantity, product, line_id, level):
+        components = []
+        total = 0
+        for line in bom.bom_line_ids:
+            line_quantity = (bom_quantity / (bom.product_qty or 1.0)) * line.product_qty
+            if line._skip_bom_line(product):
+                continue
+            company = bom.company_id or self.env.company
+            price = line.product_id.uom_id._compute_price(line.product_id.with_context(force_company=company.id).standard_price, line.product_uom_id) * line_quantity
+            if line.child_bom_id:
+                factor = line.product_uom_id._compute_quantity(line_quantity, line.child_bom_id.product_uom_id) / line.child_bom_id.product_qty
+                sub_total = self._get_price(line.child_bom_id, factor, line.product_id)
+            else:
+                sub_total = price
+            sub_total = self.env.company.currency_id.round(sub_total)
+            components.append({
+                'prod_id': line.product_id.id,
+                'prod_name': line.product_id.display_name,
+### INJECTED DATA
+                'prod_desc': line.product_id.description,
+                'prod_revi': line.product_id.engineering_revision,
+                'prod_stat': line.product_id.state,
+### INJECTED DATA
+                'code': line.child_bom_id and line.child_bom_id.display_name or '',
+                'prod_qty': line_quantity,
+                'prod_uom': line.product_uom_id.name,
+                'prod_cost': company.currency_id.round(price),
+                'parent_id': bom.id,
+                'line_id': line.id,
+                'level': level or 0,
+                'total': sub_total,
+                'child_bom': line.child_bom_id.id,
+                'phantom_bom': line.child_bom_id and line.child_bom_id.type == 'phantom' or False,
+                'attachments': self.env['mrp.document'].search(['|', '&',
+                    ('res_model', '=', 'product.product'), ('res_id', '=', line.product_id.id), '&', ('res_model', '=', 'product.template'), ('res_id', '=', line.product_id.product_tmpl_id.id)]),
+
+            })
+            total += sub_total
+        return components, total
+    
+    def _get_pdf_line(self, bom_id, product_id=False, qty=1, child_bom_ids=[], unfolded=False):
+
+        data = self._get_bom(bom_id=bom_id, product_id=product_id.id, line_qty=qty)
+
+        def get_sub_lines(bom, product_id, line_qty, line_id, level):
+            data = self._get_bom(bom_id=bom.id, product_id=product_id.id, line_qty=line_qty, line_id=line_id, level=level)
+            bom_lines = data['components']
+            lines = []
+            for bom_line in bom_lines:
+                lines.append({
+                    'name': bom_line['prod_name'],
+### INJECTED DATA
+                    'revi': bom_line['prod_revi'],
+                    'desc': bom_line['prod_desc'],
+                    'stat': bom_line['prod_stat'],
+### INJECTED DATA
+                    'type': 'bom',
+                    'quantity': bom_line['prod_qty'],
+                    'uom': bom_line['prod_uom'],
+                    'prod_cost': bom_line['prod_cost'],
+                    'bom_cost': bom_line['total'],
+                    'level': bom_line['level'],
+                    'code': bom_line['code'],
+                    'child_bom': bom_line['child_bom'],
+                    'prod_id': bom_line['prod_id']
+                })
+                if bom_line['child_bom'] and (unfolded or bom_line['child_bom'] in child_bom_ids):
+                    line = self.env['mrp.bom.line'].browse(bom_line['line_id'])
+                    lines += (get_sub_lines(line.child_bom_id, line.product_id, bom_line['prod_qty'], line, level + 1))
+            if data['operations']:
+                lines.append({
+                    'name': _('Operations'),
+### INJECTED DATA
+                    'revi': "",
+                    'desc': "",
+                    'stat': "",
+### INJECTED DATA
+                    'type': 'operation',
+                    'quantity': data['operations_time'],
+                    'uom': _('minutes'),
+                    'bom_cost': data['operations_cost'],
+                    'level': level,
+                })
+                for operation in data['operations']:
+                    if unfolded or 'operation-' + str(bom.id) in child_bom_ids:
+                        lines.append({
+                            'name': operation['name'],
+### INJECTED DATA
+                            'revi': "",
+                            'desc': "",
+                            'stat': "",
+### INJECTED DATA
+                            'type': 'operation',
+                            'quantity': operation['duration_expected'],
+                            'uom': _('minutes'),
+                            'bom_cost': operation['total'],
+                            'level': level + 1,
+                        })
+            return lines
+
+        bom = self.env['mrp.bom'].browse(bom_id)
+        product = product_id or bom.product_id or bom.product_tmpl_id.product_variant_id
+        pdf_lines = get_sub_lines(bom, product, qty, False, 1)
+        data['components'] = []
+        data['lines'] = pdf_lines
+        return data
+
+    
+
+### OVERRIDDEN STANDARD METHODS 
+
