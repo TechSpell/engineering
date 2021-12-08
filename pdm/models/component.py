049f2aae819dc5da78f21e030aa13a7376c6aca3
Fabio Colognesi <fabio.colognesi@didotech.com>
2021-12-08 18:54:06 +0100

    Avoids issues with product variants.

--- pdm/models/component.py
+++ pdm/models/component.py
@@ -1082,6 +1082,8 @@ class plm_component(models.Model):
                         'userid': self._uid,
                         }
                 self.env['plm.logging'].create(values)
+        else:
+            ret=super(plm_component, self).create(vals)
         return ret
 
     def write(self, vals):
