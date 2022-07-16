55acd06f42aa30a4f13084a5bcdad9bd577ebc99
Fabio Colognesi <fabio.colognesi@didotech.com>
2022-07-16 11:23:07 +0200

    Changed temporary repository to a writable one.

--- pdm/models/base_client.py
+++ pdm/models/base_client.py
@@ -1316,7 +1316,8 @@ class plm_config_settings(models.Model):
         d0 = datetime.datetime(2000, 1, 1)
         d1 = datetime.datetime.now()
         midmod = (d1-d0).total_seconds()
-        return "/tmp/{table}-{uid}-{midmod}.csv".format(table=tablename, uid=self._uid, midmod=midmod)
+        base_dir = tools_config.get('data_dir', os.path.join(tools_config['root_path'], 'filestore'))
+        return "{base}/{table}-{uid}-{midmod}.csv".format(base=base_dir, table=tablename, uid=self._uid, midmod=midmod)
 
     @api.model
     def GetTableData(self, request="", default=None):
